ef8cf8a1d1714c62be583cf4cf571fab43361080
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-12-28 21:27:35 -0700

    new customer route

--- backend/routes/customers.js
+++ backend/routes/customers.js
@@ -16,4 +16,17 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/api/new-customer', (req, res) => {
+    let sql = 'call newCustomer(?,?,?,?,?,?,?,?)';
+    let query = db.query(sql, [req.body.p_first_name, req.body.p_last_name, req.body.p_email, req.body.p_address, req.body.p_city, req.body.p_state, req.body.p_zip, req.body.p_phone]
+        , (err, results) => {
+            if (err) {
+                console.error(err.message);
+            }
+            console.log('+++++++++++query: ', query);
+            console.log(results);
+            res.json(results);
+        });
+});
+
 module.exports = router;
