3fc3e3157c8bfea84652a1be0351f4325dabdbd6
Clarissa Reeve <clarissa.reeve@gmail.com>
2021-01-04 21:13:23 -0700

    broke total workorders

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 const api = {
-    test: () => axios.get('/test')
-    , getCustomers: () => axios.get('/api/customers/')
+    getCustomers: () => axios.get('/api/customers/')
+    , postNewCust: () => axios.post('/api/customers/new-customer')
     , getStatsWO: () => axios.get('/api/stats/getcountWO')
+    , getStatsRsn: () => axios.get('./api/stats/getcountRsn')
+    , getStatsInv: () => axios.get('./api/stats/getTotInv')
+    , getInv: () => axios.get('./api/inventory/getinv')
+    , getTechs: () => axios.get('./api/techs/')
+    , postNewTech: () => axios.post('/api/techs/new-tech')
 };
 
 export default api;
