69a9e4eab39719ea116704c0abcf50358526530f
Walter Guerrero <wsguerrero@gmail.com>
2021-01-04 19:09:00 -0700

    customer table now pulling data from API

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import API from '../util/api.js';
+// import API from '../util/api.js';
 
 // Route components
 import Login from './Login/Login';
@@ -14,11 +14,11 @@ import NewCustomer from './New-Customer/NewCustomer';
 
 class App extends Component {
     render() {
-        console.log('this.props: ', this.props);
+        // console.log('this.props: ', this.props);
 
-        API.getStatsWO().then(response => {
-            console.log('response:', response);
-        });
+        // API.getStatsWO().then(response => {
+        //     console.log('response:', response);
+        // });
 
         return (
             <BrowserRouter>
