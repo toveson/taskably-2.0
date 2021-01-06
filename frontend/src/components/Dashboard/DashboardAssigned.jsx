1d097eb0999ea71006006e87619ac22519de7c7b
Clarissa Reeve <clarissa.reeve@gmail.com>
2021-01-05 22:39:24 -0700

    dashboard is live data

--- frontend/src/components/Dashboard/DashboardAssigned.jsx
+++ frontend/src/components/Dashboard/DashboardAssigned.jsx
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class DashboardAssigned extends Component {
-    render() {
-        return (
-            <div>
-                {/* DashboardAssigned */}
-                <div className="notification is-primary has-text countWOA">
-                    {/* <p className="title is-1">{{ countWoA }}</p> */}
-                    <p className="title is-1">countWoA</p>
-                    <p className="subtitle is-4">Assigned Work Orders</p>
-                </div>
+function DashboardAssigned(props) {
+    // console.log('props: ', props);
+    const stats = props.stats;
+    // console.log(stats);
+    return (
+        <div>
+            {/* DashboardAssigned */}
+            <div className="notification is-primary has-text countWOA">
+                <p className="title is-1">{stats[0]?.count}</p>
+                {/* <p className="title is-1">countWoA</p> */}
+                <p className="subtitle is-4">Assigned Work Orders</p>
             </div>
-        );
-    }
+        </div>
+    );
 }
+
+DashboardAssigned.propTypes = { stats: PropTypes.any };
 
 export default DashboardAssigned;
