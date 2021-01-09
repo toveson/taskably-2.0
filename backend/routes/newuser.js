24334481171565130a682ddf1eeb4172d8e5cdbe
Trever O <toveson9522@gmail.com>
2021-01-08 19:41:50 -0700

    fixed newuser route

--- backend/routes/newuser.js
+++ backend/routes/newuser.js
@@ -1,40 +1,37 @@
-const db = require('./config/connection');
+const db = require('../config/connection.js');
+const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
-const express = require('express');
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 
 // make a new user
-app.post('/newuser', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    let email = 'some@email.com';
-    let role = 'Manager';
+router.post('/newuser', (req, res) => {
+    let p_username = req.body.username;
+    let p_password = req.body.password;
+    let p_email = req.body.email;
+    let p_role = req.body.role;
 
     // hashing password
-    bcrypt.hash(password, saltRounds, function (err, hash) {
+    bcrypt.hash(p_password, saltRounds, function (err, hash) {
         // Store hash in users table in database.
         let user = {
-            username,
-            email,
-            password: hash,
-            role
+            p_username,
+            p_email,
+            p_password: hash,
+            p_role
         };
 
         let sql = 'call newUser(?,?,?,?)';
-        db.query(sql, [user.username, user.email, user.password, user.role]
-            , (err, rows) => {
+        db.query(sql, [user.p_username, user.p_email, user.p_password, user.p_role],
+            (err, rows) => {
                 if (err) {
                     console.error(err.message);
                     return res.status(500).send(err);
                 }
                 console.log(rows);
-                res.send(rows);
             });
         console.log('hit new user');
         res.end();
     });
 });
+
+module.exports = router;
