883bde4d51d06d1fe4cf55e0f13d2f3d6e2b96f8
Clarissa Reeve <clarissa.reeve@gmail.com>
2021-01-13 18:26:46 -0700

    heroku scripts added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require('express');
 const app = express();
+const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
     cors: {
@@ -14,6 +15,11 @@ const routes = require('./routes/index.js');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '../frontend/build/index.html'));
+});
 
 io.on('connection', function (socket) {
     console.log('New User Connected');
