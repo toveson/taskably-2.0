64c6fa578701d8009e6f3fcf0f7ebf27c9f14245
Walter Guerrero <wsguerrero@gmail.com>
2021-01-16 09:11:56 -0700

    burger menu enabled on mobile screen size

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,79 +1,86 @@
-import React, { Component } from 'react';
+import React from 'react';
+// import React, { Component } from 'react';
 import taskablyLogo from '../../assets/taskably.png';
+import { Link } from 'react-router-dom';
 
-class Navbar extends Component {
-    render() {
-        return (
+const Navbar = () => {
+    function toggleBurgerMenu() {
+        document.querySelector('.navbar-menu').classList.toggle('is-active');
+    }
+    // class Navbar extends Component {
+    //     render() {
+    return (
 
-            <nav className='navbar has-shadow is-fixed-top'>
+        <nav className='navbar has-shadow is-fixed-top'>
+            {/* <div className='container' id='navbar-background'> */}
+            <div className='navbar-brand'>
+                {/* <!-- Logo, tagline and navbar-burger --> */}
+                <Link className='navbar-item' width="250" to="dashboard">
+                    <img src={taskablyLogo} alt="taskably company logo" />
+                </Link>
 
-                <div className='navbar-brand'>
-                    {/* <!-- Logo, tagline and navbar-burger --> */}
-                    <a className='navbar-item' width="250" href="dashboard">
-                        <img src={taskablyLogo}alt="taskably company logo" />
-                    </a>
-                    {/* <div className='navbar-burger burger'
-                        onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div> */}
-                </div>
+                <span className='navbar-burger burger' aria-label='menu' aria-expanded='false' data-target='navbarMenu'
+                    onClick={toggleBurgerMenu}>
+                    <span aria-hidden='true'></span>
+                    <span aria-hidden='true'></span>
+                    <span aria-hidden='true'></span>
+                </span>
+            </div>
 
-                <div className='navbar-menu'>
-                    {/* <!-- User name, dropdown menu --> */}
-                    <div className='navbar-start'>
-                        <div className='navbar-item'>
-                            <div>
-                                {/* <!-- motto --> */}
-                                <h3 className="is-italic">Your Way Of Doing Things</h3>
-                            </div>
+            <div className='navbar-menu'>
+                {/* <!-- User name, dropdown menu --> */}
+                <div className='navbar-start'>
+                    <div className='navbar-item'>
+                        <div>
+                            {/* <!-- motto --> */}
+                            <h3 className="is-italic">Your Way Of Doing Things</h3>
                         </div>
                     </div>
+                </div>
 
-                    <div className='navbar-end'>
-                        <div className='navbar-item has-dropdown is-hoverable'>
-                            <div className='navbar-link'>
-                                {/* <!-- User name --> */}
-                                {/* {{!-- Username/Email needs to pull --}} */}
-                                {/* {{ user }} */}
+                <div className='navbar-end'>
+                    <div className='navbar-item has-dropdown is-hoverable'>
+                        <div className='navbar-link'>
+                            {/* <!-- User name --> */}
+                            {/* {{!-- Username/Email needs to pull --}} */}
+                            {/* {{ user }} */}
                                 user
 
-                            </div>
-                            <div className='navbar-dropdown is-right'>
-                                <a className="navbar-item" href="new-customer">
-                                    <div>
-                                        <span className='icon is-small'>
-                                            <i className="fas fa-user-edit"></i>
-                                        </span>
+                        </div>
+                        <div className='navbar-dropdown is-right'>
+                            <a className="navbar-item" href="new-customer" onClick={toggleBurgerMenu}>
+                                <div>
+                                    <span className='icon is-small'>
+                                        <i className="fas fa-user-edit"></i>
+                                    </span>
                                 Create New Customer
-                                    </div>
-                                </a>
-                                <a className="navbar-item" href="inventory">
-                                    <div>
-                                        <span className="icon is small">
-                                            <i className='fa fa-book'></i>
-                                        </span>
+                                </div>
+                            </a>
+                            <a className="navbar-item" href="inventory" onClick={toggleBurgerMenu}>
+                                <div>
+                                    <span className="icon is small">
+                                        <i className='fa fa-book'></i>
+                                    </span>
                                 Inventory
-                                    </div>
-                                </a>
-                                <a className="navbar-item" href='/'>
-                                    <div>
-                                        <span className="icon is-small">
-                                            <i className="fa fa-sign-out"></i>
-                                        </span>
+                                </div>
+                            </a>
+                            <a className="navbar-item" href='/' onClick={toggleBurgerMenu}>
+                                <div>
+                                    <span className="icon is-small">
+                                        <i className="fa fa-sign-out"></i>
+                                    </span>
                                 Sign Out
-                                    </div>
-                                </a>
-                            </div>
+                                </div>
+                            </a>
                         </div>
                     </div>
                 </div>
-            </nav>
+            </div>
+        </nav >
 
 
-        );
-    }
-}
+    );
+};
+// }
 
 export default Navbar;
